f09f3f5fe78e3ec2e9eeeadf5b8068c9b0f23fca
Alik1604 <rudenko.albert.n@gmail.com>
2021-08-10 12:28:53 +0300

    Add clear button

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-
 const gridContainer = document.querySelector('#grid-container');
+const buttonClear = document.querySelector('#clear')
+buttonClear.addEventListener('click', clearColor)
 for (let i = 0; i < 256; i++){
    const div = document.createElement('div');
    div.classList.add('div_grid');
    div.addEventListener('mouseover', setColor)
-   div.addEventListener('click', setColor)
    gridContainer.appendChild(div)
 }
 
 function setColor (element){
    element.currentTarget.style.backgroundColor = 'red';
+}
+
+function clearColor (){
+   const amountDivs = Array.from (gridContainer.childNodes);
+   amountDivs.shift()
+   amountDivs.forEach( e => e.style.backgroundColor = 'aqua')
 }
