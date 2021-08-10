6fbcb13bb3aec12e6cf8e50cbfd7f31f53886a69
Alik1604 <rudenko.albert.n@gmail.com>
2021-08-10 13:29:18 +0300

    add flex grid

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const gridContainer = document.querySelector('#grid-container');
 const buttonClear = document.querySelector('#clear')
+const squerOfContainer = 256*256;
+let lengthOfdiv = prompt();
+let squerOfdiv = lengthOfdiv * lengthOfdiv;
+let numberOfDivs = squerOfContainer/squerOfdiv;
+
 buttonClear.addEventListener('click', clearColor)
-for (let i = 0; i < 256; i++){
+for (let i = 0; i < numberOfDivs; i++){
    const div = document.createElement('div');
    div.classList.add('div_grid');
+   div.style.width = lengthOfdiv+'px';
+   div.style.height =  lengthOfdiv+'px';
    div.addEventListener('mouseover', setColor)
    gridContainer.appendChild(div)
 }
